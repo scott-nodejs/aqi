0adaacb13b45a5ca86cabfef6731b6a586b7b6b2
lucong <lucong@kingsoft.com>
2021-01-15 19:03:19 +0800

    update : 增加爬取失败汇报

--- src/main/java/com/aqi/service/imp/NoResultServiceImpl.java
+++ src/main/java/com/aqi/service/imp/NoResultServiceImpl.java
@@ -4,7 +4,9 @@ import com.aqi.entity.NoResult;
 import com.aqi.mapper.city.NoResultMapper;
 import com.aqi.service.NoResultService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NoResultServiceImpl extends ServiceImpl<NoResultMapper, NoResult> implements NoResultService {
     @Override
     public void insertNoResult(NoResult noResult) {
